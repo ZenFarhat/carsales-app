7116d71077424b96cd6387847d79d666672ded66
ZenFarhat <s3777800@student.rmit.edu.au>
2021-03-06 12:52:34 +1100

    post requests working

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,15 @@ import axios from "axios";
 
 function App() {
   const [ads, getAds] = useState([]);
+  const [adData, setAdData] = useState({
+    title: "",
+    make: "",
+    transmission: "",
+    cylinders: "",
+    price: "",
+    style: "",
+    image: "",
+  });
 
   const url = "http://localhost:5000/ads/";
 
@@ -17,11 +26,44 @@ function App() {
       .catch((error) => console.error(`Error: ${error}`));
   };
 
+  const handleSubmit = async (e) => {
+    await axios.post(url, adData);
+    setAdData({
+      title: "",
+      make: "",
+      transmission: "",
+      cylinders: "",
+      price: "",
+      style: "",
+      image: "",
+    });
+  };
+
   useEffect(() => {
     getAllAds();
   }, []);
   return (
     <div className='App'>
+      <form onSubmit={handleSubmit}>
+        <h1>Submit New Ad</h1>
+        <input
+          name='title'
+          variant='outlined'
+          label='title'
+          value={setAdData.title}
+          onChange={(e) => setAdData({ ...adData, title: e.target.value })}
+          placeholder='ad-title'
+        />
+        <input
+          name='price'
+          variant='outlined'
+          label='price'
+          value={setAdData.price}
+          onChange={(e) => setAdData({ ...adData, price: e.target.value })}
+          placeholder='ad-price'
+        />
+        <button type='submit'>Submit</button>
+      </form>
       {ads.map((ad) => {
         return (
           <div className='ad' key={ad._id}>
